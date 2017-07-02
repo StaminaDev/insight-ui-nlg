fe49f0bfb57f5708746d3a684cc41bc6faedea9c
strataghyst <g.h.degraaf@gmail.com>
2017-07-02 14:30:33 +0200

    make ready for Gulden

--- public/src/js/controllers/currency.js
+++ public/src/js/controllers/currency.js
@@ -20,7 +20,7 @@ angular.module('insight.currency').controller('CurrencyController',
 
         if (this.symbol === 'USD') {
           response = _roundFloat((value * this.factor), 2);
-        } else if (this.symbol === 'mLTC') {
+        } else if (this.symbol === 'mNLG') {
           this.factor = 1000;
           response = _roundFloat((value * this.factor), 5);
         } else if (this.symbol === 'lites') {
@@ -47,7 +47,7 @@ angular.module('insight.currency').controller('CurrencyController',
         Currency.get({}, function(res) {
           $rootScope.currency.factor = $rootScope.currency.bitstamp = res.data.bitstamp;
         });
-      } else if (currency === 'mLTC') {
+      } else if (currency === 'mNLG') {
         $rootScope.currency.factor = 1000;
       } else if (currency === 'lites') {
         $rootScope.currency.factor = 1000000;
